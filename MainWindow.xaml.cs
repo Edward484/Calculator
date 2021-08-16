2824392a32700fce46ea595369894fc92310d179
Edward484 <selea.eduard@gmail.com>
2021-08-16 14:22:47 +0300

    functionality finsished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,13 +18,134 @@ namespace Calculator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Division,
+        Multiplication
+    }
+
     public partial class MainWindow : Window
     {
+        double lastNumber;
+        double result;
+        SelectedOperator SelectedOperator;
         public MainWindow()
         {
             InitializeComponent();
 
-            resultLabel.Content = "1500";
+            ACButton.Click += AcButton_Click;
+            plusPerOneButton.Click += PlusPerOneButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalButton.Click += EqualButton_Click;
+            pointButton.Click += PointButton_Click;
+        }
+
+        private void PointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+            }
+            resultLabel.Content = $"{resultLabel.Content}.";
+
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out newNumber))
+            {
+                switch (SelectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = Math.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = Math.Substract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = Math.Mulitply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = Math.Divide(lastNumber, newNumber);
+                        break;
+                }
+            }
+            resultLabel.Content = result.ToString();
+
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            double tempNumber;
+            if (double.TryParse(resultLabel.Content.ToString(), out tempNumber))
+            {
+                tempNumber = tempNumber / 100;
+                if(lastNumber != 0)
+                {
+                    tempNumber = lastNumber;
+                }
+                resultLabel.Content = tempNumber.ToString();
+            }
+        }
+
+        private void PlusPerOneButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * (-1);
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+            result = 0;
+            lastNumber = 0;
+        }
+
+        private void operationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                resultLabel.Content = 0;
+            }
+
+            if (sender == multiplyButton)
+            {
+                SelectedOperator = SelectedOperator.Multiplication;
+            }
+            if(sender == divideButton)
+            {
+                SelectedOperator = SelectedOperator.Division;
+            }
+            if(sender == addButton)
+            {
+                SelectedOperator = SelectedOperator.Addition;
+            }
+            if(sender == substractButton)
+            {
+                SelectedOperator = SelectedOperator.Substraction;
+            }
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = int.Parse((sender as Button).Content.ToString());
+
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
         }
     }
+
 }
---
+++ Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator
+{
+    class Math
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Mulitply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            if(b == 0)
+            {
+                MessageBox.Show("Division by 0 is not allowed", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+            return a / b;
+        }
+    }
+}
